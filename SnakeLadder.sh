6c0b317e902086eb6e564fac96799c46c8cf562d
Mikhil Desai <mikhildesai@gmail.com>
2020-06-09 23:09:01 +0530

    Played till 100 winning point

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -3,37 +3,52 @@
 echo "Welcome to Snake Ladder game!!"
 position=0
 echo "STARTING WITH POSITION " $position
+next=0
+WINNING_POINT=100
 
-total=0
-count=0
+while true
+do
+	dice=$[(RANDOM%6) +1 ]
+	echo "Dice" $dice
 
-dice=$[(RANDOM%6) +1 ]
-echo "Dice" $dice
+	option=$[(RANDOM%3)]
+	curr_position=$position
 
-option=$[(RANDOM%3)]
-curr_position=$position
+		case $option in 
+						0 ) #stay at same position
+							curr_position=$curr_position
+							echo "Stay here"
+								;;
 
-	case $option in 
-					0 ) #stay at same position
-						curr_position=$curr_position
-						echo "Stay here"
-							;;
+						1 ) #ladder up
+							curr_position=$[(curr_position+dice)]
+							echo "Ladder"
+								;;
 
-					1 ) #ladder up
-						curr_position=$[(curr_position+dice)]
-						echo "Ladder"
-							;;
+						2 ) #snake down
+							curr_position=$[(curr_position-dice)]
+							echo "Snake"
+								;;
+		esac
 
-					2 ) #snake down
-						curr_position=$[(curr_position-dice)]
-						echo "Snake"
-							;;
-	esac
+		#if (( $curr_position < 0))
+		#then
+		#		curr_position=$position
+		#fi
 
-	if (( $curr_position < 0))
+	echo "current position " $curr_position
+
+	next=$[(next+curr_position)]
+	if (( $next < 0 ))
 	then
-			curr_position=$position
+			next=$position
 	fi
 
-echo $curr_position
-		
+	if (( $next == $WINNING_POINT ))
+	then
+			break;
+	fi
+
+	echo "next position" $next
+	echo " "
+done
