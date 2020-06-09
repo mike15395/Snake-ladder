84fe000a542790fb5556b462e885827f4dd4419b
Mikhil Desai <mikhildesai@gmail.com>
2020-06-09 18:00:54 +0530

    Options added for stay/ladder/sanke using RANDOM function

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -4,4 +4,22 @@ echo "Welcome to Snake Ladder game!!"
 position=0
 
 dice=$[(RANDOM%6) +1 ]
-echo $dice
+#echo $dice
+
+option=$[(RANDOM%3)]
+
+	case $option in 
+					0 ) #stay at same position
+						position=$position
+							;;
+
+					1 ) #ladder up
+						position=$[(position+dice)]
+							;;
+
+					2 ) #snake down
+						position=$[(position-dice)]
+							;;
+	esac
+
+echo $position
