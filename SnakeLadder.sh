3fd2c972b3e2c285c87408b7ac57cecae3dde9ec
Mikhil Desai <mikhildesai@gmail.com>
2020-06-09 20:58:29 +0530

    checked for negative values

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -2,24 +2,38 @@
 
 echo "Welcome to Snake Ladder game!!"
 position=0
+echo "STARTING WITH POSITION " $position
+
+total=0
+count=0
 
 dice=$[(RANDOM%6) +1 ]
-#echo $dice
+echo "Dice" $dice
 
 option=$[(RANDOM%3)]
+curr_position=$position
 
 	case $option in 
 					0 ) #stay at same position
-						position=$position
+						curr_position=$curr_position
+						echo "Stay here"
 							;;
 
 					1 ) #ladder up
-						position=$[(position+dice)]
+						curr_position=$[(curr_position+dice)]
+						echo "Ladder"
 							;;
 
 					2 ) #snake down
-						position=$[(position-dice)]
+						curr_position=$[(curr_position-dice)]
+						echo "Snake"
 							;;
 	esac
 
-echo $position
+	if (( $curr_position < 0))
+	then
+			curr_position=$position
+	fi
+
+echo $curr_position
+		
