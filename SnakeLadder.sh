954388e0c5c30abb4caec5208461a003d8908b28
Mikhil Desai <mikhildesai@gmail.com>
2020-06-10 18:46:20 +0530

    Multiplayer added and code refactored into functions

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,62 +1,116 @@
 #/bin/bash -x
 
 echo "Welcome to Snake Ladder game!!"
-position=0
+position=0		# intial position before game starts
 echo "STARTING WITH POSITION " $position
 
+WIN_POINT=100	#constant
 next=0
-WINNING_POINT=100
-count=0
 
-while (( $next !=WINNING_POINT ))
-do
+#functions created for each action : dice,roll,option selection,
+#playing,next position
 
-((++count))
-	echo "Roll" $count
+function dice()
+{
 	dice=$[(RANDOM%6) +1 ]
 	echo "Dice" $dice
 
+}
+
+function option()
+{
 	option=$[(RANDOM%3)]
-	curr_position=$position
+}
 
-		case $option in 
-						0 ) #stay at same position
-							curr_position=$curr_position
-							echo "Stay here"
-								;;
+function play()
+{
+				curr_position=$position
 
-						1 ) #ladder up
-							curr_position=$[(curr_position+dice)]
-							echo "Ladder"
-								;;
+				case $option in 
+								0 ) #stay at same position
+									curr_position=$curr_position
+									echo "Stay here"
+										;;
 
-						2 ) #snake down
-							curr_position=$[(curr_position-dice)]
-							echo "Snake"
-								;;
-		esac
+								1 ) #ladder move up
+										echo "Ladder"
+										curr_position=$[(curr_position+dice)]
+										;;
 
-		#if (( $curr_position < 0))
-		#then
-		#		curr_position=$position
-		#fi
+								2 ) #snake move down
+									curr_position=$[(curr_position-dice)]
+									echo "Snake"
+										;;
+				esac
 
-	echo "current position " $curr_position
+			echo "position" $curr_position
+}
 
+function nextpos()
+{
 	next=$[(next+curr_position)]
+
 	if (( $next < 0 ))
 	then
 			next=$position
-	elif (( $next > $WINNING_POINT ))
+	elif (( $next > 100 ))
 	then
 			next=$[(next-curr_position)]
 	fi
 
-	#if (( $next == $WINNING_POINT ))
-	#then
-	#		break;
-	#fi
+	echo "next position " $next
+}
 
-	echo "next position" $next
+
+#Main code starts from here
+count1=0
+count2=0
+
+while (( $next != WIN_POINT ))	#loops till win point 100 is reached
+do
+	flag=1						#flag is used to switch between player1 and player2
+
+	if (( $flag == 1))
+	then
+
+
+		echo "Player1"
+
+		dice										#each player rolls dice
+		option									#then checks options :snake/ladder/stay
+		play										#gets to next position
+		((count1++))							#roll count for each player
+		echo "Roll" $count1
+		nextpos
+			if (( $next == WIN_POINT))		#condition check if win point reached
+			then									#yes then exit and print winner
+					echo "Player1 WON!"
+					break;
+			fi
+		flag=0
+		echo " "
+	fi
+
 	echo " "
+
+	if (( $flag == 0))					# the same goes for player2 as well
+	then
+
+		echo "Player 2"
+
+		dice
+		option
+		play
+		((count2++))
+		echo "Roll"$count2
+		nextpos
+			if (( $next == WIN_POINT))
+			then
+					echo "Player2 WON!"
+					break;
+			fi
+		flag=1
+		echo " "
+	fi
+
 done
